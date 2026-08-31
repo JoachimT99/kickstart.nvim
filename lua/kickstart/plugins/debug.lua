7ff974c3e482b4513f23d222d7be7a454609ae98
Joachim Thomassen <joth@dips.no>
2026-08-31 13:53:45 +0200

    Remove Go DAP config (delve/nvim-dap-go setup)
    I don't use Go, so drop the delve ensure_installed entry and the dap-go setup.

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -44,7 +44,6 @@ require('mason-nvim-dap').setup {
   -- online, please don't ask me how to install them :)
   ensure_installed = {
     -- Update this to ensure that you have the debuggers for the langs you want
-    'delve',
   },
 }
 
@@ -87,15 +86,6 @@ dapui.setup {
 dap.listeners.after.event_initialized['dapui_config'] = dapui.open
 dap.listeners.before.event_terminated['dapui_config'] = dapui.close
 dap.listeners.before.event_exited['dapui_config'] = dapui.close
-
--- Install golang specific config
-require('dap-go').setup {
-  delve = {
-    -- On Windows delve must be run attached or it crashes.
-    -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-    detached = vim.fn.has 'win32' == 0,
-  },
-}
 
 -- Install python specific config
 require('dap-python').setup 'uv'
